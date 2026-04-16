6204a22a9195563663d379de25956bfa460a154f
Abner Pereira <abnerluiz.88@gmail.com>
2026-04-16 00:05:16 -0300

    Types and Values v1

--- files/main.lua
+++ files/main.lua
@@ -1,1 +1,29 @@
-print("Olá Lua!")
+--[[
+	Comentário de Bloco
+	print("Olá Lua!")
+]]
+-- Comentário de Linha
+print("Digite seu nome: ")
+
+-- Global Variables
+Nome = io.read("*l") --"*l" >> ler até o final da linha
+
+-- Local Variables
+local sobreNome = "Santos"
+
+print("Seu nome é", Nome, "!")
+
+-- Types and Values
+-- Function Type >> Return type name
+print("Valor:", sobreNome, "| Tipo:", type(sobreNome)) -- string
+print("Valor:", 100, "| Tipo:", type(100))             -- number
+print("Valor:", true, "| Tipo:", type(true))           -- boolean
+print("Valor:", nil, "| Tipo:", type(nil))             -- null
+
+local show = print
+print("Valor:", show, "| Tipo:", type(show)) -- function
+
+--[[
+Marcador - Onde paramos...
+https://www.lua.org/pil/2.html
+]]
